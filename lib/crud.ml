91f786eaa2ed4970013acf5b9130a0fa69a1c61a
Davi Suga <daviciencia1@gmail.com>
2022-04-25 13:08:51 -0300

    added endpoints to read and add logs

--- lib/crud.ml
+++ lib/crud.ml
@@ -1,16 +1,29 @@
 open Opium
 
-type log = {
-  stack : string list;
-  time : float;
-  message : string;
-  level : string;
-  origin : string;
-}
-[@@deriving yojson]
+let ( let* ) = Lwt.bind
 
-let read_client_logs =
+open Storage
+
+let status =
   App.get "/" (fun _req ->
       Lwt.return (Response.of_plain_text "up and running..."))
 
-let start_server () = App.empty |> read_client_logs |> App.run_multicore
+let read_client_logs =
+  App.get "/logs" (fun _request ->
+      let* logs = Storage.read_all_logs () in
+      let json = [%to_yojson: log_stored list] logs in
+      Lwt.return (Response.of_json json))
+
+let post_client_log =
+  App.post "/logs" (fun req ->
+      let* input_log = Request.to_json_exn req in
+      let log =
+        match log_stored_of_yojson input_log with
+        | Ok log -> log
+        | Error e -> raise (Invalid_argument e)
+      in
+      print_string log.message;
+      Lwt.return (Response.of_plain_text "ok"))
+
+let start_server () =
+  App.empty |> read_client_logs |> post_client_log |> App.run_multicore
