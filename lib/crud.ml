0de895388f417913e8cdaa513f6423453551b606
Davi Suga <daviciencia1@gmail.com>
2022-04-28 11:13:32 -0300

    fixed post error handling

--- lib/crud.ml
+++ lib/crud.ml
@@ -21,15 +21,15 @@ let read_client_logs =
 let post_client_log =
   App.post "/logs" (fun req ->
       let* input_log_option = Request.to_json req in
-      
-      match input_log_option |> Option.map log_of_yojson  with
-      | Some (Ok log) -> 
+
+      match input_log_option |> Option.map log_of_yojson with
+      | Some (Ok log) ->
           let* r = DB.insert_log_db log in
           Lwt.return (Response.of_plain_text "ok")
+      | None -> Lwt.return (Response.of_plain_text "No json here bro")
+      | Some (Error e) ->
+          Lwt.return (Response.of_plain_text @@ "Invalid json input: " ^ e))
 
-      | None  -> Lwt.return (Response.of_plain_text "No json here bro")
-      | Some ( Error e ) -> Lwt.return (Response.of_plain_text @@ "Invalid json input: " ^ e)
-   )
 let start_server () =
   App.empty
   |> Utils.with_msg "Starting server at http://localhost:3000"
