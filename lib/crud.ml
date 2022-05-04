0c11dc084fde02d4ba4f3e97d6db0ce5ca94a7cd
Davi Suga <daviciencia1@gmail.com>
2022-05-04 18:19:41 -0300

    commented Opium code and added simple Dream code

--- lib/crud.ml
+++ lib/crud.ml
@@ -1,40 +1,44 @@
-open Opium
-open Lwt
+(* open Opium *)
+open! Lwt
+open Utils
 
 let ( let* ) = Lwt.bind
 
 open Storage
 
-let status =
-  App.get "/" (fun _req ->
-      Lwt.return (Response.of_plain_text "Up and running..."))
+(* let status =
+   App.get "/" (fun _req ->
+       Lwt.return (Response.of_plain_text "Up and running...")) *)
 
 let make_log_jsons = [%to_yojson: log_stored list]
 
-let read_client_logs =
-  App.get "/logs" (fun _ ->
-      print_endline "Reading client logs";
-      let* logs = DB.read_all_logs () in
-      let logs = logs |> make_log_jsons in
+(* let read_client_logs =
+     App.get "/logs" (fun _ ->
+         print_endline "Reading client logs";
+         let* logs = DB.read_all_logs () in
+         let logs = logs |> make_log_jsons in
 
-      print_endline "Client logs read";
-      Lwt.return @@ Response.of_json logs)
+         print_endline "Client logs read";
+         Lwt.return @@ Response.of_json logs)
 
-let post_client_log =
-  App.post "/logs" (fun req ->
-      let* input_log_option = Request.to_json req in
+   let post_client_log =
+     App.post "/logs" (fun req ->
+         let* input_log_option = Request.to_json req in
 
-      match input_log_option |> Option.map log_of_yojson with
-      | Some (Ok log) ->
-          let* r = DB.insert_log_db log in
-          Lwt.return (Response.of_plain_text "ok")
-      | None -> Lwt.return (Response.of_plain_text "No json here bro")
-      | Some (Error e) ->
-          Lwt.return (Response.of_plain_text @@ "Invalid json input: " ^ e))
+         match input_log_option |> Option.map log_of_yojson with
+         | Some (Ok log) ->
+             let* _ = DB.insert_log_db log in
+             Lwt.return (Response.of_plain_text "ok")
+         | None -> Lwt.return (Response.of_plain_text "No json here bro")
+         | Some (Error e) ->
+             Lwt.return (Response.of_plain_text @@ "Invalid json input: " ^ e)) *)
 
-let start_server () =
-  App.empty
-  |> App.port (Sys.getenv "PORT" |> int_of_string)
-  |> Utils.with_msg @@ "Starting server at http://localhost:"
-     ^ Sys.getenv "PORT"
-  |> status |> post_client_log |> read_client_logs |> App.run_multicore
+let port = env "PORT" "8081"
+
+(*
+   let start_server () =
+     App.empty
+     |> App.port (port |> int_of_string)
+     |> Utils.with_msg @@ "Starting server at http://localhost:" ^ port
+     |> status |> post_client_log |> read_client_logs |> App.run_multicore *)
+let start_server () = Dream.run (fun _ -> Dream.html "Good morning, world!")
